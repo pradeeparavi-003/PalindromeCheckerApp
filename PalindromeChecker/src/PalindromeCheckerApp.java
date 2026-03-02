a427906046aeb2628b77af6fa33d1c708e3c2394
pradeeparavi-003 <pradeeparavi003@gmail.com>
2026-03-02 10:31:26 +0530

    UC5: Stack-Based Palindrome Checker

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -4,22 +4,35 @@
  *
  */
 
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String original = "madam";
-        String reversed = "";
+import java.util.Scanner;
+import java.util.Stack;
+public class PalindromeCheckerApp
+{
+    public static void main (String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        Stack<Character> stack = new Stack<>();
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
         }
-
-        if (original.equals(reversed)) {
-            System.out.println("The string is a Palindrome.");
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("The string is a palindrome.");
         } else {
-            System.out.println("The string is NOT a Palindrome.");
+            System.out.println("The string is not a palindrome.");
         }
+        sc.close();
     }
-
 }
+
 
 
